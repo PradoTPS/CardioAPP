68ea0e45ea18d60c87b656131ad68bc9b0ddbabe
PradoTPS <prado.tps@gmail.com>
2018-11-19 19:08:40 -0200

    Decision system implemented

--- src/pages/first/first.ts
+++ src/pages/first/first.ts
@@ -11,6 +11,7 @@ export class FirstPage {
 
   cardHeader: any;
   step: number;
+  block: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.cardHeader = "Paciente gestante ou em pós-parto?"
@@ -25,6 +26,27 @@ export class FirstPage {
   private callAgain(header, step){
     this.cardHeader = header;
     this.step = step;
+
+    switch(this.block){
+      case 1:
+        this.cardHeader = "Interrompa investigação de Miocardiopatia Periparto!";
+        this.step = 11;
+        break;
+      case 2:
+        this.cardHeader = "Interrompa investigação de Miocardiopatia Periparto!";
+        this.step = 12;
+        break;
+      case 3:
+        this.cardHeader = "Solicitar Eletrocardiograma (ECG) e exames laboratoriais."
+        this.step = 13;
+        break;
+    }
+
+    this.block = 0;
+  }
+
+  private setBlock(number){
+    this.block = number;
   }
 
   private goHome(){
